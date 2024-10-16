7f44630b683f1bfbd81d41c94e1a4f984e438661
Anonymousmashle <20233599@s.ubaguio.edu>
2024-10-16 13:02:57 +0800

    octa16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,20 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from "./header/header.component";
 import { BodyComponent } from "./body/body.component";
 import { FooterComponent } from "./footer/footer.component";
+import { LayoutComponent } from './layout/layout.component';
+import { UserAccountsComponent } from "./user-accounts/user-accounts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    NavbarComponent, HeaderComponent, BodyComponent, FooterComponent],
+    NavbarComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    LayoutComponent,
+    UserAccountsComponent],
+    
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { HeaderComponent } from "../header/header.component";
+import { BodyComponent } from "../body/body.component";
+import { FooterComponent } from "../footer/footer.component";
+import { UserAccountsComponent } from "../user-accounts/user-accounts.component";
 
 @Component({
-  selector: 'app-layout',
+  selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,
+    NavbarComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    UserAccountsComponent],
+    
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
