077af5936733fcb1fb8fc6fbbdb2ff14670af336
Reymart Samdao <20233599@s.ubaguio.edu>
2024-11-25 23:17:23 +0800

    inital

--- Routing-Example/src/app/app.routes.ts
+++ Routing-Example/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'profile', component: ProfileComponent},
+];
